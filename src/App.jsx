819f8188ce4dc460c5d546362c59d0dc67720764
Herlangga <herlangga.maulani@gmail.com>
2024-05-25 21:27:30 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { Route, Routes } from "react-router-dom"
+import Home from "./pages/Home"
+import About from "./pages/About"
+
+function App() {
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Home/>}></Route>
+        <Route path="/about" element={<About/>}></Route>
+      </Routes>
+    </>
+  )
+}
+
+export default App
